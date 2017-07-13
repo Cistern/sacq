d592faf7df67d8cb840f2bb7d511403d5fa4a22a
Preetam Jinka <pj@preet.am>
2017-07-13 00:40:11 -0400

    add more test cases

--- test/role.cc
+++ test/role.cc
@@ -61,9 +61,10 @@ TEST_CASE( "Follower responds to a leader heartbeat", "[role]" ) {
 	uint64_t ts = 1e9;
 	role.periodic(ts);
 
-	auto leader_ack = std::make_unique<LeaderActiveMessage>();
-	leader_ack->source = 1;
-	role.handle_leader_active(ts, *leader_ack);
+	auto leader_active = std::make_unique<LeaderActiveMessage>();
+	leader_active->id = 1;
+	leader_active->source = 1;
+	role.handle_leader_active(ts, *leader_active);
 
 	// Role should still be Follower.
 
@@ -74,4 +75,44 @@ TEST_CASE( "Follower responds to a leader heartbeat", "[role]" ) {
 	REQUIRE( ack_msg->id == 2 );
 	REQUIRE( ack_msg->seq == 0 );
 	REQUIRE( ack_msg->round == 0 );
+}
+
+TEST_CASE( "Follower ignores other leader heartbeat", "[role]" ) {
+	TestRegistry reg;
+
+	std::unique_ptr<LeaderActiveAck> ack_msg;
+	reg.m_send_to_index = std::function<void(int, const Message*)>([&](int index, const Message* msg) {
+		REQUIRE( msg->type == MSG_LEADER_ACTIVE_ACK );
+
+		auto leader_ack = std::make_unique<LeaderActiveAck>();
+		*leader_ack = *static_cast<const LeaderActiveAck*>(msg);
+		ack_msg = std::move(leader_ack);
+	});
+
+	Role role(reg, 2, 2);
+
+	uint64_t ts = 1e9;
+	role.periodic(ts);
+
+	auto leader_active = std::make_unique<LeaderActiveMessage>();
+	leader_active->id = 1;
+	leader_active->source = 1;
+	role.handle_leader_active(ts, *leader_active);
+
+	// Role should still be Follower.
+
+	REQUIRE( role.state() == Follower );
+	REQUIRE( role.round() == 0 );
+
+	REQUIRE( ack_msg != nullptr );
+	REQUIRE( ack_msg->id == 2 );
+	REQUIRE( ack_msg->seq == 0 );
+	REQUIRE( ack_msg->round == 0 );
+
+	ack_msg = nullptr;
+	leader_active->id = 2;
+	leader_active->source = 2;
+	role.handle_leader_active(ts, *leader_active);
+
+	REQUIRE( ack_msg == nullptr );
 }
